756b6a62c69c6d588f83ab3af62b009c710946f0
Anna Conaway <annaconaway19@gmail.com>
2018-07-03 22:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  
+}
